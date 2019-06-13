d66148458a582bf981eef207e9aaeb1c772ac2f2
CorentinDoue <doue.coco@hotmail.fr>
2019-06-13 18:39:15 +0200

    feat(frontend): add performance test

--- frontend/src/Home/States/index.js
+++ frontend/src/Home/States/index.js
@@ -1,14 +1,28 @@
 import { Container } from '@material-ui/core';
-import { pubsubClient } from 'ApolloConfig';
+import Button from '@material-ui/core/Button';
+import { gqlClient, pubsubClient } from 'ApolloConfig';
 import NewStateComponent from 'Home/NewState';
 import StateComponent from 'Home/State';
-import { SUBSCRIBE_DELETED_STATES, SUBSCRIBE_STATES } from 'Queries/States';
+import {
+  DELETE_STATE,
+  SAVE_STATE,
+  SUBSCRIBE_DELETED_STATES,
+  SUBSCRIBE_STATES,
+} from 'Queries/States';
 import React, { Component } from 'react';
 import 'Home/States/states.css';
 
 class StatesComponent extends Component {
   state = {
     states: this.props.states,
+    startTime: new Date().getTime(),
+    perf: {
+      newState: [],
+      updateState: [],
+      deleteState: [],
+    },
+    perfState: 'no',
+    count: 0,
   };
 
   updateStates = async result => {
@@ -35,6 +49,84 @@ class StatesComponent extends Component {
     }
   };
 
+  testPerf = async () => {
+    if (this.state.perfState === 'no') {
+      await this.setState({ perfState: 'new', count: 0 });
+    }
+    const currentTime = new Date().getTime();
+    if (this.state.perfState === 'new') {
+      try {
+        gqlClient.mutate({
+          mutation: SAVE_STATE,
+          variables: {
+            state: { state: 'newState' },
+          },
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    } else if (this.state.perfState === 'update') {
+      try {
+        gqlClient.mutate({
+          mutation: SAVE_STATE,
+          variables: {
+            state: { id: this.state.states[0].id, state: 'updatedState' },
+          },
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    } else if (this.state.perfState === 'delete') {
+      try {
+        gqlClient.mutate({
+          mutation: DELETE_STATE,
+          variables: {
+            id: this.state.states[0].id,
+          },
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    this.setState({ startTime: currentTime });
+  };
+
+  computePerf = endTime => {
+    if (this.state.perfState === 'new') {
+      this.setState({
+        perf: {
+          ...this.state.perf,
+          newState: [...this.state.perf.newState, endTime - this.state.startTime],
+        },
+        perfState: 'update',
+      });
+    } else if (this.state.perfState === 'update') {
+      this.setState({
+        perf: {
+          ...this.state.perf,
+          updateState: [...this.state.perf.updateState, endTime - this.state.startTime],
+        },
+        perfState: 'delete',
+      });
+    } else if (this.state.perfState === 'delete') {
+      this.setState({
+        perf: {
+          ...this.state.perf,
+          deleteState: [...this.state.perf.deleteState, endTime - this.state.startTime],
+        },
+        perfState: 'new',
+        count: this.state.count + 1,
+      });
+    }
+    if (this.state.count > 9) {
+      this.setState({
+        perfState: 'no',
+      });
+    } else {
+      this.testPerf();
+    }
+  };
+
   componentDidMount = () => {
     pubsubClient
       .subscribe({
@@ -44,7 +136,11 @@ class StatesComponent extends Component {
       .subscribe(
         {
           next: data => {
+            const currentTime = new Date().getTime();
             this.updateStates(data);
+            if (this.state.perfState !== 'no') {
+              this.computePerf(currentTime);
+            }
           },
         },
         () => console.log('error'),
@@ -58,7 +154,11 @@ class StatesComponent extends Component {
       .subscribe(
         {
           next: data => {
+            const currentTime = new Date().getTime();
             this.deleteState(data);
+            if (this.state.perfState !== 'no') {
+              this.computePerf(currentTime);
+            }
           },
         },
         () => console.log('error'),
@@ -68,12 +168,63 @@ class StatesComponent extends Component {
     const { states } = this.state;
     console.log('state changed');
     return (
-      <Container className="container">
-        {states.map(state => (
-          <StateComponent id={`state` + state.id} key={state.id} selectedState={state} />
-        ))}
-        <NewStateComponent id="new_state" />
-      </Container>
+      <>
+        <Container className="container">
+          {states.map(state => (
+            <StateComponent id={`state` + state.id} key={state.id} selectedState={state} />
+          ))}
+          <NewStateComponent id="new_state" />
+        </Container>
+        <Container className="container">
+          <Button variant="contained" color="secondary" onClick={() => this.testPerf()}>
+            Test Perf
+          </Button>
+        </Container>
+        <div>
+          {this.state.perf.newState.length && (
+            <p>
+              Mean delay (ms) to create a state over {this.state.perf.newState.length} repeat :{' '}
+              {this.state.perf.newState.reduce((sum, time) => sum + time, 0) /
+                this.state.perf.newState.length}{' '}
+              ms, min :{' '}
+              {this.state.perf.newState.reduce(
+                (min, time) => (time < min ? time : min),
+                100000000000000000,
+              )}{' '}
+              ms, max :{' '}
+              {this.state.perf.newState.reduce((max, time) => (time > max ? time : max), 0)} ms
+            </p>
+          )}
+          {this.state.perf.updateState.length && (
+            <p>
+              Mean delay (ms) to modify a state over {this.state.perf.updateState.length} repeat :{' '}
+              {this.state.perf.updateState.reduce((sum, time) => sum + time, 0) /
+                this.state.perf.updateState.length}{' '}
+              ms, min :{' '}
+              {this.state.perf.updateState.reduce(
+                (min, time) => (time < min ? time : min),
+                100000000000000000,
+              )}{' '}
+              ms, max :{' '}
+              {this.state.perf.updateState.reduce((max, time) => (time > max ? time : max), 0)}{' '}
+            </p>
+          )}
+          {this.state.perf.deleteState.length && (
+            <p>
+              Mean delay (ms) to delete a state over {this.state.perf.deleteState.length} repeat :{' '}
+              {this.state.perf.deleteState.reduce((sum, time) => sum + time, 0) /
+                this.state.perf.deleteState.length}{' '}
+              ms, min :{' '}
+              {this.state.perf.deleteState.reduce(
+                (min, time) => (time < min ? time : min),
+                100000000000000000,
+              )}{' '}
+              ms, max :{' '}
+              {this.state.perf.deleteState.reduce((max, time) => (time > max ? time : max), 0)}{' '}
+            </p>
+          )}
+        </div>
+      </>
     );
   }
 }
